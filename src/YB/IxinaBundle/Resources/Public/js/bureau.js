f57c636440e671385aafbea6f901a4c5c794e66d
garrytopper <yannickpro64@gmail.com>
2017-12-07 22:13:21 +0100

    travail sur le fond d ecran de AppIxina

--- src/YB/IxinaBundle/Resources/Public/js/bureau.js
+++ src/YB/IxinaBundle/Resources/Public/js/bureau.js
@@ -1,5 +1,5 @@
-var largeurFenetre = $('window').width ;
-var hauteurFenetre = $('window').height;
+var largeurFenetre = $(window).width() ;
+var hauteurFenetre = $(window).height() ;
 $('body').css('width', largeurFenetre).css('hauteurFenetre', hauteurFenetre);
 
 /* chargement de la page effectué */
