16b22882ac1d20f7edb14169f273ce004f407b38
Garrytopper <yannickpro64@gmail.com>
2017-12-08 09:29:51 +0100

    reglages des fonds ecrans

--- src/YB/IxinaBundle/Resources/Public/js/bureau.js
+++ src/YB/IxinaBundle/Resources/Public/js/bureau.js
@@ -1,8 +1,12 @@
 var largeurFenetre = $(window).width() ;
 var hauteurFenetre = $(window).height() ;
-$('body').css('width', largeurFenetre).css('hauteurFenetre', hauteurFenetre);
+$('body').css('width', largeurFenetre).css('height', hauteurFenetre);
+$('body').css('background-size', largeurFenetre/2 );
+$('#bureau').hide();
 
 /* chargement de la page effectué */
 $(function(){
 	
+	$('#bureau').css('margin', 'auto');
+	$('#bureau').fadeIn(1000);
 });
