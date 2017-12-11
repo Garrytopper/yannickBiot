17bff66dc1a40487dba1af4317b6a51a77784106
Garrytopper <yannickpro64@gmail.com>
2017-12-11 17:16:00 +0100

    rajout du bundle user : ok

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new YB\DashBundle\YBDashBundle(),
             new YB\IxinaBundle\YBIxinaBundle(),
+            new YB\UserBundle\YBUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
