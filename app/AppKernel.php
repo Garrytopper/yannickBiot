8f3faad2c2b513557cac1d4dc95618053a66fa12
garrytopper <yannickpro64@gmail.com>
2017-12-13 18:59:38 +0100

    ajout de fosuserbundle dans le kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new YB\DashBundle\YBDashBundle(),
             new YB\IxinaBundle\YBIxinaBundle(),
             new YB\UserBundle\YBUserBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
