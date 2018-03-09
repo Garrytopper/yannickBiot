bca759f31338c3af5089debdbce1ef23d9b43b51
garrytopper <yannickpro64@gmail.com>
2018-03-09 12:10:45 +0100

    mise en page

--- src/YB/IxinaBundle/Resources/Public/js/blocDossier.js
+++ src/YB/IxinaBundle/Resources/Public/js/blocDossier.js
@@ -1,4 +1,9 @@
-$('#blocDossier').animate({width: '95%'}, 1000);
+$('#pageGauche').hide();
+$('#pageDroite').hide();
+$('#blocDossier').animate({width: '95%'}, 1000, function(){
+    $('#pageGauche').fadeIn(1000);
+    $('#pageDroite').fadeIn(1000);
+});
 
 $(function(){
     
