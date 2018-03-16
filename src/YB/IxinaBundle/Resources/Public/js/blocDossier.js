2304547b3bd1254528c6c40aa0cdda49cea1104a
garrytopper <yannickpro64@gmail.com>
2018-03-16 10:04:58 +0100

    ...

--- src/YB/IxinaBundle/Resources/Public/js/blocDossier.js
+++ src/YB/IxinaBundle/Resources/Public/js/blocDossier.js
@@ -1,10 +1,23 @@
-$('#pageGauche').hide();
-$('#pageDroite').hide();
 $('#blocDossier').animate({width: '95%'}, 1000, function(){
     $('#pageGauche').fadeIn(1000);
     $('#pageDroite').fadeIn(1000);
 });
 
 $(function(){
-    
+    $('#bloc2D').dblclick(function(){
+        $('#pageGauche').hide(function(){
+            $('#pageGaucheCoordonnees').fadeIn(1000);
+        });
+        $('#pageDroite').hide(function(){
+            $('#pageDroiteCoordonnees').fadeIn(1000);
+        });
+    });
+    $('#iconeAnnuler').click(function(){
+        $('#pageGaucheCoordonnees').hide(function(){
+            $('#pageGauche').fadeIn(1000);
+        });  
+        $('#pageDroiteCoordonnees').hide(function(){
+            $('#pageDroite').fadeIn(1000);
+        }); 
+    });
 });
